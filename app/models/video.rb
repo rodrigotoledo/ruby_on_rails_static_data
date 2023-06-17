12e6ba679a4fca17c634f93d78f3c6ee886f2720
Rodrigo Toledo <rodrigo.toledo@gliconline.com.br>
2023-06-17 09:08:58 -0300

    broadcasting latest video

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,9 @@
 class Video < ApplicationRecord
+  after_create :broadcasting_to_latest_video
+  after_update :broadcasting_to_latest_video
+  after_destroy :broadcasting_to_latest_video
+  protected
+  def broadcasting_to_latest_video
+    broadcast_update_to 'latest_video', target: 'latest_video', partial: 'videos/latest_video'
+  end
 end
